2fae53e6e1e3f3cf01eceb5af784b87063c838b9
Andrius Putna <fordnox@gmail.com>
2016-01-21 22:44:13 +0200

    Adding tests

--- src/Ladesk.php
+++ src/Ladesk.php
@@ -1,13 +1,56 @@
 <?php
 
+namespace Ladesk;
+
+use GuzzleHttp\Client;
+
 class Ladesk
 {
 
-    /**
-     * Ladesk constructor.
-     */
-    public function __construct()
+    public function __construct($url, $key)
     {
-        //something to do
+        $this->api_key = $key;
+        $this->client = new Client(array(
+            'base_uri' => $url.'/api/index.php',
+            'timeout' => 5,
+        ));
+    }
+
+    public function companies()
+    {
+        $result = $this->call('companies');
+        return $result['companies'];
+    }
+
+    public function agent($id)
+    {
+        return $this->call('agents/' . $id);
+    }
+
+    public function agentOnlineStatus()
+    {
+        $result = $this->call('onlinestatus/agents');
+        return $result['agentsOnlineStates'];
+    }
+
+    private function call($url)
+    {
+        $params = array(
+            'query' => array(
+                'handler' => $url,
+                'apikey' => $this->api_key,
+            )
+        );
+        $response = $this->client->request('GET', '', $params);
+        $code = $response->getStatusCode();
+        if ($code != 200) {
+            throw new \ErrorException('status occurred: ' . $code);
+        }
+        $json = (string)$response->getBody();
+        $decoded = json_decode($json, 1);
+        if (isset($decoded['response']['status']) && $decoded['response']['status'] == 'ERROR') {
+            throw new \ErrorException('error occurred: ' . $decoded['response']['errormessage']);
+        }
+        return $decoded['response'];
     }
 }
